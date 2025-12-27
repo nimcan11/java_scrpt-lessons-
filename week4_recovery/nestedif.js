b9d3ffb9c80c2992882c592a36e653068d487f9a
nimcan abdi <abdin9951@gmail.com>
2025-12-27 17:29:00 +0300

    week 4 and day11

---
+++ week4_recovery/nestedif.js
@@ -0,0 +1,50 @@
+// nesed if statement 
+let qiime_guud=180;
+// another  variable 
+let Aradey=true;
+let qd=true
+let qd_ardey=50;
+let qd_qiimo=20;
+
+if(Aradey===true){
+    qiime_guud=qiime_guud-qd_ardey;
+    if(qd===true){
+        qiime_guud=qiime_guud-qd_qiimo;
+    }
+    console.log(`qimahgau waa $${qiime_guud}`)
+}else{
+    console.log("wax qiime dhimisa ma lihid")
+}
+
+// marks checet 
+let marks = 90;
+if(marks > 50){
+    console.log("wad gudubtey")
+    if(marks >= 80){
+        console.log("waxaad tahey A+")
+    }
+}else{
+    console.log("waad dhacdey")
+}
+
+// ternary method in if condition 
+num = 20;
+num > 10 ? console.log("waa number weyn") : console.log("waa number yar");
+// another example of ternary method
+
+let ardey = false;
+let Age = 20;
+ardey == true ? console.log("waxaad tahey ardet") : Age >= 20 ? console.log("waxaad tahey nin weyn") :console.log("laguma aqaansan ")
+
+// days of wee using sitch 
+let day = 3;
+switch(day){
+    case 1 :
+        console.log("maanta waa sabti")
+        break;
+        case 2:
+            console.log("maanta waa axad")
+            break;
+            case 3 :
+                console.log("maanta waa isniin!!!")
+}
