44be6d91d53160d98ddb6631c71346e208bf956e
nimcan abdi <abdin9951@gmail.com>
2026-02-09 09:56:42 +0300

    with other lessons

--- week9/week9.js
+++ week9/week9.js
@@ -1,30 +1,36 @@
-// // the map method 
-// const num =[1,2,3,4,5,6,34,56,78,12];
-// const double = num.map(num=>num*2);
-// console.log(double);
+// the map method 
+const num =[1,2,3,4,5,6,34,56,78,12];
+const double = num.map(num=>num*2);
+console.log(double);
 
-// const names=["Jaamac","Ali","Khaalid"];
-// const waaweyne = names.map(num => num.toUpperCase());
-// console.log(waaweyne)
+const names=["Jaamac","Ali","Khaalid"];
+const waaweyne = names.map(num => num.toUpperCase());
+console.log(waaweyne)
 
-// // filter method 
-// const even_num =num.filter((num) =>{
-//     return  num % 2===0;
-// })
-// console.log(even_num);
-
-// // display only those who have start a 
-// const magacaan =["Nimcaan","ayaaan","khaalid","Fatima","caaisha"];
-// const ku_bilaabma_a =magacaan.filter((magac)=>{
-//     magac.startsWith("a");
-// })
-// console.log(ku_bilaabma_a);
-
-
-/// reduce method 
-const digits=[1,2,,3,4,5,6,,7,8];
-const myreduce_digits=digits.reduce(function(accu,index,array){
-console.log(`Accumaltor:${accu}`) 
-return accu + 1;
+// filter method 
+const even_num =num.filter((num) =>{
+    return  num % 2===0;
 })
+console.log(even_num);
+
+// display only those who have start a 
+const magacaan =["Nimcaan","ayaaan","khaalid","Fatima","caaisha"];
+const ku_bilaabma_a =magacaan.filter((magac)=>{
+    magac.startsWith("a");
+})
+console.log(ku_bilaabma_a);
+
+
+// reduce method 
+const digits = [1, 2, 3, 4];
+
+const total = digits.reduce(function(accu, current,index){
+  console.log("Accumulator:", accu);
+  console.log("Current value:", current);
+  console.log(`index:${index}`)
+  return accu + current;
+}, 0); // initial value
+
+console.log("Final result:", total);
+
 // console.log(myreduce_digits)
