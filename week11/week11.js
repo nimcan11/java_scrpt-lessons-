26466ed9f794919485305cb55488cf149b488cd8
nimcan abdi <abdin9951@gmail.com>
2026-02-16 14:43:14 +0300

    checking is an array

--- week11/week11.js
+++ week11/week11.js
@@ -8,7 +8,14 @@ console.log(title.textContent);
 
 // getting elemnts by class
 const class1=document.getElementsByClassName("lists");
+// you can loop if you want to display clearly
 console.log(class1)
 for(let i =0 ;i<class1.length;i++){
 console.log(class1[i])
 }
+// getting elements by tag name 
+const paragraph=document.getElementsByTagName("h1");
+console.log(paragraph);
+// is not an array it is an object we check using type of 
+console.log(typeof paragraph);
+console.log(Array.isArray(paragraph)); // false 
