01dd87d906291125579cd455abb6fb3d241fc132
nimcan abdi <abdin9951@gmail.com>
2026-02-16 14:34:04 +0300

    accessing elements by class name

--- week11/week11.js
+++ week11/week11.js
@@ -5,3 +5,10 @@ console.dir(document)
 // selecting elements 
 const title = document.getElementById("title");
 console.log(title.textContent);
+
+// getting elemnts by class
+const class1=document.getElementsByClassName("lists");
+console.log(class1)
+for(let i =0 ;i<class1.length;i++){
+console.log(class1[i])
+}
