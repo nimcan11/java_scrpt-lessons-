0033ed21e19cfabb4481af07af0197b2666ab49d
nimcan abdi <abdin9951@gmail.com>
2026-02-26 06:07:16 +0300

    Accessing elemnts

--- week11makeup/main.js
+++ week11makeup/main.js
@@ -2,3 +2,7 @@
 
 const my_name = document.querySelector(".my_name");
 console.log(my_name.textContent);
+
+// accesing elemnts 
+my_name.textContent = "Nimcan Odowa";
+console.log(my_name.textContent);
