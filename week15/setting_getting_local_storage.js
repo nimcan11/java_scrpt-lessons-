f9bad0d83376f2693dded634a6e4f910388ab053
nimcan abdi <abdin9951@gmail.com>
2026-03-03 06:32:09 +0300

    getting and setting local storage using strinfy

--- week15/setting_getting_local_storage.js
+++ week15/setting_getting_local_storage.js
@@ -4,3 +4,6 @@ const proggraming_languages = [
         {Name:"python",type:"High level language",Role:"Ai"},
           {Name:"C++",type:"High level language",Role:"desktop"},
 ];
+localStorage.setItem("Proggraming_languages",JSON.stringify(proggraming_languages));
+const save_data = localStorage.getItem("Proggraming_languages");
+console.log(save_data);
