803ec00bc4fc8fe3a3c262aece052006f5c8f8d7
nimcan abdi <abdin9951@gmail.com>
2026-03-07 08:47:44 +0300

    Alx spread operator basics

---
+++ week19/spreadoperator.js
@@ -0,0 +1,8 @@
+// basics 
+// most of spread operators used for copying and compining the arrays
+const fruits = ["lemon","Avacado","mango"];
+const vagies = ["carrot","palermo"];
+const fruits_copy = [...vagies,...fruits];
+console.log(fruits_copy)
+
+
